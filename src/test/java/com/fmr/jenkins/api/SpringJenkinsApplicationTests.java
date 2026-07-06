832e23805a12bc4b740fd5af0425d41c7816486d
Anantha <javabykanna@gmail.com>
2026-07-06 17:56:43 -0400

    second commit

--- src/test/java/com/fmr/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/fmr/jenkins/api/SpringJenkinsApplicationTests.java
@@ -1,12 +1,22 @@
 package com.fmr.jenkins.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
     @Test
     void contextLoads() {
+        logger.info("Test case executing...");
+        logger.info("Test case executed second log statement...");
+        assertEquals(true, true);
     }
 }
